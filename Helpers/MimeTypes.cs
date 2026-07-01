33972a049478143de77927f0f8a2757e1a93f968
Robert B <asi93gf03jn@gmail.com>
2026-07-01 11:04:37 -0400

    MimeTypes.cs: Looks at file's extension and returns the label for it. application/octet-stream just treats it as a generic download.

---
+++ Helpers/MimeTypes.cs
@@ -0,0 +1,28 @@
+using System.IO;
+
+namespace ProInternal.Helpers
+{
+    public static class MimeTypes
+    {
+        public static string GetMimeType(string filePath)
+        {
+            var ext = Path.GetExtension(filePath).ToLowerInvariant();
+            return ext switch
+            {
+                ".pdf" => "application/pdf",
+                ".png" => "image/png",
+                ".jpg" => "image/jpeg",
+                ".jpeg" => "image/jpeg",
+                ".gif" => "image/gif",
+                ".txt" => "text/plain",
+                ".csv" => "text/csv",
+                ".zip" => "application/zip",
+                ".doc" => "application/msword",
+                ".docx" => "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+                ".xls" => "application/vnd.ms-excel",
+                ".xlsx" => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                _ => "application/octet-stream"
+            };
+        }
+    }
+}
